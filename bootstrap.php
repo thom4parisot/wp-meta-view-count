12ac32d582210dd69c98cd187bd2257942867a7d
Oncle Tom <thomas@oncle-tom.net>
2013-02-09 12:01:41 +0100

    Bumping version

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
 Plugin Name: Meta View Count
 Description: Simple counter for posts and pages, working through cache mechanism (like W3 Total Cache, Hyper Cache etc.).
 Author: Oncle Tom
-Version: 1.0.0-dev
+Version: 1.1.0
 Author URI: http://case.oncle-tom.net/
 Plugin URI: http://wordpress.org/extend/plugins/meta-view-count/
 
